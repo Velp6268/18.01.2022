0777d6a05c7baab56c7911457f877221ee31da40
user <user@C1011>
2022-01-18 11:39:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication50/ConsoleApplication50.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+
+double velp_recur(double N, double y)
+
+{
+    y = y * (N / (N - 1)) * (N / (N + 1));
+
+    if (N > 2) { return velp_recur(N - 2, y); }
+    return y;
+}
+//---------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "rus");
+
+    int N;
+    cout << "Введите четное число: ";
+    cin >> N;
+
+    cout << "y= " << velp_recur(N, 1);
+    return 0;
+}
